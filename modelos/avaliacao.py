5271ca43bb0decce397c4e6f7921ba07b220df05
Leandro <ljborges76@gmail.com>
2024-01-03 16:37:46 -0300

    Adicionando docstring e finalizando a aplicação

---
+++ modelos/avaliacao.py
@@ -0,0 +1,5 @@
+class Avaliacao:
+    """Representa uma avaliação de um restaurante e suas características."""
+    def __init__(self, cliente, nota):
+        self._cliente = cliente
+        self._nota = nota
