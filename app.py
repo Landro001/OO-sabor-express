60bc8bdec4e9da9976c65072653fb4f9c59599e5
Leandro <ljborges76@gmail.com>
2024-01-04 16:44:49 -0300

    Adicionando cardápio e colocando em prática herança e polimorfismo

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 5)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+sobremesa_pudim = Sobremesa('Pudim', 10.0,'Pudinzinho docinho' , 'pedaço', 'medio')
+sobremesa_pudim.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(sobremesa_pudim)
+
 
 def main():
     """Inicializa a aplicação"""
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
